0ae5498618b324dd02f934cfecb6bba09a51abe8
Johannes Grimm <johannes.grimm@ondamedia.de>
2023-04-21 17:42:39 +0200

    Init nx-workspace

---
+++ nx-test/src/app/shop/shop.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-test-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss'],
+})
+export class ShopComponent {}
